7bb1d507bde27298eacc143a9b83c9dafaaf0590
Aman <amandeep1994@msn.com>
2022-06-20 18:46:31 +0100

    Milestone 3

--- src/main/java/com/abidevel/openbanking/contract/model/Transaction.java
+++ src/main/java/com/abidevel/openbanking/contract/model/Transaction.java
@@ -2,6 +2,14 @@ package com.abidevel.openbanking.contract.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.abidevel.openbanking.contract.model.enumeration.TransactionType;
 
 import lombok.AllArgsConstructor;
@@ -17,8 +25,12 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity(name = "Transaction")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private TransactionType type;
     private Date date;
     private Long accountNumber;
